79228f152e8ab4e976793663355c417e9ef0c13f
IT21305214 <99234637+IT21305214@users.noreply.github.com>
2022-02-08 10:44:04 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   int i;
+   float mark, sum = 0, avg;
+   printf("Input marks of two subjects: ");
+   for(i = 0; i < 2; i++)
+   {
+      scanf("%f", &mark):
+      sum = sum + mark:
+   }
+   avg = sum / 2;
+   printf("\nAverage mark of two subjects = %0.2f", avg);
+   getch();
+   
   
   return 0;
 }
